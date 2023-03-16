33690bcfd3e4f2b1694d3d20256a4056de2a87c9
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-03-15 15:30:47 -1200

    Welcome page

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace LogicalPrograms
+{
+    class Program
+    {
+        public static void Main(string[] args) 
+        {
+            //Console.WriteLine("Welcome to Logical Problems");
+            //FibonacciSeries.DisplayFibonacciSeries();
+            //PerfectNumber.DisplayPerfectNumber();
+            //ReverseNumber.ReverseTheNumber();
+            //PrimeNumber.FindPrimeNumber();
+            CouponNumber.FindCouponNumber();
+        }
+    }
+}
