f042f2050705a0282f48149fc5a0b041ad872f49
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-03-15 15:35:51 -1200

    find prime number

---
+++ PrimeNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class PrimeNumber
+    {
+        public static void FindPrimeNumber()
+        {
+            Console.WriteLine("Please Enter the number");
+            int num=Convert.ToInt32(Console.ReadLine());
+
+            for (int i=2; i<(num/2); i++) 
+            {
+                int ans = num % i;
+                if (ans == 0)
+                {
+                    Console.WriteLine("The {0} is not a prime number" ,num);
+                }
+                else 
+                {
+                    Console.WriteLine("The {0} is a prime number", num);
+                }
+
+            }
+        }
+    }
+}
